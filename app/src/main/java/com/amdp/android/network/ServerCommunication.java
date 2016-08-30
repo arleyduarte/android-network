fd505f29a62854410186eabde62d1c97ff107060
Arley <arley.duarte@zyght.com>
2016-08-30 09:20:20 -0300

    network

--- app/src/main/java/com/amdp/android/network/ServerCommunication.java
+++ app/src/main/java/com/amdp/android/network/ServerCommunication.java
@@ -6,7 +6,6 @@ package com.amdp.android.network;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
